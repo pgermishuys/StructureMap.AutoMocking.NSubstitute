e80b809d34507b2063131992e5120dfb69e7a828
pgermishuys <pieter@pieterg.com>
2013-05-06 21:12:40 +0200

    Initial Commit

---
+++ src/StructureMap.AutoMocking.NSubstitute/NSubstituteFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace StructureMap.AutoMocking.NSubstitute
+{
+	public class NSubstituteFactory
+	{
+		private readonly MethodInfo mockMethod;
+
+		public NSubstituteFactory()
+		{
+			Assembly nSubstitute = Assembly.Load("NSubstitute");
+			mockMethod = nSubstitute.GetType("NSubstitute.Substitute").GetMethods().First(x => x.Name == "For" &&
+																						  x.GetGenericArguments().Length == 1 &&
+																						  x.IsGenericMethod &&
+																						  x.IsStatic);
+			if (mockMethod == null)
+				throw new InvalidOperationException("Unable to find Type NSubstitute.Substitute.For<T> in assembly " + nSubstitute.Location);
+		}
+
+		public object CreateMock(Type type)
+		{
+			var genericMockMethod = mockMethod.MakeGenericMethod(type);
+			var mockedtype = genericMockMethod.Invoke(null, new object[] { new object[] { } });
+			return mockedtype;
+		}
+
+		public object CreateMockThatCallsBase(Type type, object[] args)
+		{
+			var genericMockMethod = mockMethod.MakeGenericMethod(type);
+			var mockedtype = genericMockMethod.Invoke(null, new object[] { args });
+			return mockedtype;
+		}
+	}
+}
---
+++ src/StructureMap.AutoMocking.NSubstitute/NSubstituteServiceLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StructureMap.AutoMocking.NSubstitute
+{
+	public class NSubstituteServiceLocator : ServiceLocator
+	{
+		private readonly NSubstituteFactory _mocks = new NSubstituteFactory();
+
+		public T PartialMock<T>(params object[] args) where T : class
+		{
+			return (T)_mocks.CreateMockThatCallsBase(typeof(T), args);
+		}
+
+		public object Service(Type serviceType)
+		{
+			return _mocks.CreateMock(serviceType);
+		}
+
+		public T Service<T>() where T : class
+		{
+			return (T)_mocks.CreateMock(typeof(T));
+		}
+	}
+}
